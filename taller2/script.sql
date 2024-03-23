55d5a1cba08b2eccd3966290d029857a540c5b20
iinaki <llorensinaki22@gmail.com>
2024-03-23 19:23:49 -0300

    pks y fks

---
+++ taller2/script.sql
@@ -0,0 +1,37 @@
+select * from teams where team = 'ARGENTINA';
+
+drop table if exists school_teams;
+drop table if exists school_matches;
+
+create table school_teams (
+team text not null constraint pk_teams primary key, 
+players_used int not null, 
+avg_age float not null, 
+possession float not null, 
+games int not null,
+goals int not null, 
+assists int not null, 
+cards_yellow int not null, 
+cards_red int not null
+);
+
+create table school_matches (
+team1 text not null references school_teams(team), 
+team2 text not null references school_teams(team), 
+goals_team1 int not null, 
+goals_team2 int not null, 
+stage text not null,
+constraint pk_matches primary key (team1, team2, stage)
+);
+
+COPY school_teams
+FROM '/teams.csv'
+DELIMITER ';'
+CSV HEADER --para indicar que saltee la primera l´ınea
+ENCODING 'LATIN1';
+
+COPY school_matches
+FROM '/matches.csv'
+DELIMITER ';'
+CSV HEADER --para indicar que saltee la primera l´ınea
+ENCODING 'LATIN1';
